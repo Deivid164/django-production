d49af2c44b27587ce6ed3e6dcc68888c836e3ba4
Deivid <dgonzalezm6@gmail.com>
2024-11-08 21:47:05 -0600

    segundo commit

--- server/urls.py
+++ server/urls.py
@@ -20,7 +20,7 @@ from produccion import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('arturo-product.netlify.app/', views.IniciarSesion, name='iniciarSesion'),
+    path('', views.IniciarSesion, name='iniciarSesion'),
     path('tareas/', views.Ver_tareas, name='tareas'),
     path('form_tareas/', views.Form_tareas, name='form_tarea'),
     path('logout/', views.Salir, name='logout'),
